8999204d3f16a8744eaac096405c671b7d875950
gjb1088 <39758034+gjb1088@users.noreply.github.com>
2025-08-17 15:25:10 -0400

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Burn.IT',
+  description: 'Cyberpunk MSP Website'
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
